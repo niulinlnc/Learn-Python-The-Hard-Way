1ae0853c1435cfe352a111a4d792ae67dda5b8b2
banqu <banqu@163.com>
2016-10-11 19:41:46 +0800

    注释修改

--- ex5.py
+++ ex5.py
@@ -10,5 +10,5 @@ print "Let's talk about %s." % my_name
 print "He's %d inches tall.'"% my_height
 print "Actually that's not too heavy."
 print "His teeth are usually %s depending on the coffee."% my_teeth
-# this line is tricky, try to get it exactly right
+# this line is tricky, try to get it exactly right.
 print "If I add %d,%d,and %d I get %d."%(my_age,my_height,my_weight,my_age+my_height+my_weight)
