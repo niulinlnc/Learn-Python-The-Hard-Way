23bddc6a2f318ee309ed58364962520b769e5bda
banqu <banqu@163.com>
2016-10-11 19:39:53 +0800

    第一天
    20161110

---
+++ ex5.py
@@ -0,0 +1,14 @@
+my_name='Zed A. Shaw'
+my_age=35 #not a lie
+my_height=74 #inches
+my_weight=180 #lbs
+my_eys='Blue'
+my_teeth='White'
+my_hair='Brown'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall.'"% my_height
+print "Actually that's not too heavy."
+print "His teeth are usually %s depending on the coffee."% my_teeth
+# this line is tricky, try to get it exactly right
+print "If I add %d,%d,and %d I get %d."%(my_age,my_height,my_weight,my_age+my_height+my_weight)
