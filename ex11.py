798bcfe0c5fd7d88a924f03a367e2ece94006341
banqu <banqu@163.com>
2016-10-13 20:51:44 +0800

    第四天

--- ex11.py
+++ ex11.py
@@ -2,7 +2,6 @@ print "How old are you?",
 age= raw_input()
 print "How tall are you?",
 height=raw_input()
-print "How much do you weigh?",
 weight=raw_input()
 
 print "So,you're %r old, %r tall and %r heavy." % (age,height,weight)
